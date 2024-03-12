9ffa031ee2f2013d261db305b9cd0d31d71f7b29
mirazkbr <83450502+mirazkbr@users.noreply.github.com>
2024-03-13 03:15:55 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+// public/service-worker.js
+
+const cacheName = 'moviedb';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/favicon.ico',
+  '/dist/build.js',
+  // Add other files and assets you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== cacheName) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
